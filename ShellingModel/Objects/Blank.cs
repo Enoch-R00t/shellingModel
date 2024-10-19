f89e5eefedaf073bbbc3a0c42e60042a32711c91
grady <grady.anderson@gmail.com>
2024-10-19 10:15:53 -0500

    added file input and output

--- ShellingModel/Objects/Blank.cs
+++ ShellingModel/Objects/Blank.cs
@@ -5,14 +5,16 @@ namespace ShellingModel.Objects
 {
     internal class Blank : ShellingObject
     {
+        private readonly Random rand;
         public Blank(decimal discomfortability, short xLoc, short yLoc, ref ShellingGrid shellingGrid)
             : base(discomfortability, xLoc, yLoc, ref shellingGrid)
         {
             Type = TypeEnum.Blank;
+            rand = new Random();
         }
 
-        internal override string DisplayValue => " ";
+        internal override string DisplayValue => "_";
 
-        internal override bool HappyHere() => true;
+        internal override bool HappyHere() => true; // rand.Next() > (Int32.MaxValue / 2);
     }
 }
--- ShellingModel/Program.cs
+++ ShellingModel/Program.cs
@@ -1,10 +1,26 @@
-﻿using System.Runtime.CompilerServices;
-using ShellingModel.AbstractClasses;
+﻿using ShellingModel.AbstractClasses;
 using ShellingModel.Enums;
 using ShellingModel.Objects;
 
+using System.Text.Json;
+
 namespace ShellingModel
 {
+    // serialization classes
+    public class Dimension
+    {
+        public string height { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Dimension> dimensions { get; set; }
+        public List<string> variables { get; set; }
+        public string values { get; set; }
+    }
+    // end serialization classes
+
     internal class Program
     {
         private static short gridWidth = 15;
@@ -13,19 +29,183 @@ namespace ShellingModel
         private static short currentMoves = 0;
         private static short totalMoves = 0;
         private static short gridIterations = 1;
+        private static short randomizations = 0;
 
         internal static ShellingGrid shellingGridOrig;
         internal static ShellingGrid shellingGrid;
 
+        private static Random rand;
+        private static string primingString;
+        private static List<string> variables;
+
+        private static bool UseGeneralSolution = true;
+
         internal static readonly short affinity = 1;
         static void Main(string[] args)
         {
-            SetUp();
+            GetSetupFromFile();
+
+            //SetUp();
+
+            //rand = new Random();
 
             shellingGrid = new ShellingGrid(gridWidth, gridHeight);
             shellingGridOrig = new ShellingGrid(gridWidth, gridHeight);
-            PopulateGrid();
 
+            PopulateGridFromFile();
+
+            //PopulateGridRandom();
+
+            //if (UseGeneralSolution)
+            //{
+            //    GeneralSolution();
+            //}
+            //else
+            //{
+                FindSolutionWithRandomization();
+            //}
+
+        }
+
+        private static void GetSetupFromFile()
+        {
+            string filePath = "D:\\ga_inputFiles\\20b40.txt";
+
+            var myJsonResponse = ReadInputFile<Root>(filePath);
+
+            var height = myJsonResponse.dimensions[0].height;
+            short.TryParse(height, out gridHeight);
+
+            var width = myJsonResponse.dimensions[1].width;
+            short.TryParse(width, out gridWidth);
+
+            variables = myJsonResponse.variables;
+
+            primingString = myJsonResponse.values;
+        }
+
+        public static T ReadInputFile<T>(string filePath)
+        {
+            string text = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(text);
+        }
+
+        private static void FindSolutionWithRandomization()
+        {
+            currentMoves = IterateGrid();
+            totalMoves = currentMoves;
+
+            Console.Clear();
+            shellingGridOrig.WriteGrid();
+
+            Console.WriteLine();
+            shellingGrid.WriteGrid();
+
+            // make as many moves as we can with the current configured grid
+            while (currentMoves > 0)
+            {
+                gridIterations += 1;
+
+                currentMoves = IterateGrid();
+                totalMoves += currentMoves;
+
+                Thread.Sleep(1000);
+
+                Console.Clear();
+                shellingGridOrig.WriteGrid();
+
+                Console.WriteLine();
+                shellingGrid.WriteGrid();
+            }
+
+            // are we at an optimum solution? 
+            while(!IsEverybodyHappy() && randomizations < 100)
+            {
+                // randomize
+                RandomlySwapElements();
+
+                currentMoves = IterateGrid();
+                while (currentMoves > 0)
+                {
+                    gridIterations += 1;
+
+                    currentMoves = IterateGrid();
+                    totalMoves += currentMoves;
+
+                    Thread.Sleep(1000);
+
+                    Console.Clear();
+                    shellingGridOrig.WriteGrid();
+
+                    Console.WriteLine();
+                    shellingGrid.WriteGrid();
+                }
+
+                // loop if needed
+            }
+
+            // Results and counts output
+            //--------------------------------------------------- 
+            Console.WriteLine();
+            Console.WriteLine($"Total number of elements:{gridWidth * gridHeight}");
+
+            Array values = Enum.GetValues(typeof(TypeEnum));
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine($"{values.GetValue(i)}:{shellingGrid.Count((TypeEnum)values.GetValue(i))}");
+            }
+
+            Console.WriteLine($"Total swaps :{totalMoves}");
+            Console.WriteLine($"Grid iterations:{gridIterations - 1}");
+            Console.WriteLine($"Randomizations:{randomizations}");
+
+            WriteOutputToFile();
+
+
+            Console.Read();
+        }
+
+        static void WriteOutputToFile()
+        {
+           
+            // Set a variable to the Documents path.
+            string docPath =
+              Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "output_20b40_ShellingGrid.txt"),true))
+            {
+              
+                outputFile.WriteLine("Results run:{0}", DateTime.Now);
+                outputFile.WriteLine();
+                outputFile.WriteLine("Original grid:");
+                var origGrid = shellingGridOrig.StringifyGrid();
+                foreach (string line in origGrid)
+                    outputFile.WriteLine(line);
+
+                outputFile.WriteLine();
+                outputFile.WriteLine("Resultant grid:");
+                var finalGrid = shellingGrid.StringifyGrid();
+                foreach (string line in finalGrid)
+                    outputFile.WriteLine(line);
+
+                outputFile.WriteLine();
+                outputFile.WriteLine($"Total number of elements:{gridWidth * gridHeight}");
+
+                Array values = Enum.GetValues(typeof(TypeEnum));
+                for (int i = 0; i < values.Length; i++)
+                {
+                    outputFile.WriteLine($"{values.GetValue(i)}:{shellingGrid.Count((TypeEnum)values.GetValue(i))}");
+                }
+
+                outputFile.WriteLine($"Total swaps :{totalMoves}");
+                outputFile.WriteLine($"Grid iterations:{gridIterations - 1}");
+                outputFile.WriteLine($"Randomizations:{randomizations}");
+            }
+        }
+
+        static void GeneralSolution()
+        {
             currentMoves = IterateGrid();
             totalMoves = currentMoves;
 
@@ -50,14 +230,21 @@ namespace ShellingModel
                 Console.WriteLine();
                 shellingGrid.WriteGrid();
             }
-            
+
+
+            // Results and counts output
+            //--------------------------------------------------- 
             Console.WriteLine();
             Console.WriteLine($"Total number of elements:{gridWidth * gridHeight}");
-            Console.WriteLine($"X:{shellingGrid.Count(TypeEnum.X)}");
-            Console.WriteLine($"Y:{shellingGrid.Count(TypeEnum.O)}");
-            Console.WriteLine($"Blank:{shellingGrid.Count(TypeEnum.Blank)}");
+
+            Array values = Enum.GetValues(typeof(TypeEnum));
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine($"{values.GetValue(i)}:{shellingGrid.Count((TypeEnum)values.GetValue(i))}");
+            }
+
             Console.WriteLine($"Total swaps :{totalMoves}");
-            Console.WriteLine($"Grid iterations:{gridIterations-1}");
+            Console.WriteLine($"Grid iterations:{gridIterations - 1}");
             Console.Read();
         }
 
@@ -68,6 +255,87 @@ namespace ShellingModel
             var inputVal = gridDimensions.Split(',');
             short.TryParse(inputVal[0], out gridHeight);
             short.TryParse(inputVal[1], out gridWidth);
+
+            Console.WriteLine("Use the general solution? y/n");
+            var useGeneralSolution = Console.ReadLine();
+            UseGeneralSolution = useGeneralSolution == "y" ? true : false;            
+        }
+
+        static void RandomlySwapElements()
+        {
+            short x = 0;
+            short y = 0;
+            int maxI = gridHeight * gridWidth;
+            rand = new Random();
+
+            while (y < shellingGrid.GridHeight)
+            {
+                while (x < shellingGrid.GridWidth)
+                {
+                    var left = shellingGrid.Grid[y, x];
+                    if (!left.HappyHere())
+                    {
+                        // find random new element thats not happy either
+                        var notHappyEither = false;                        
+                        short otherX = 0;
+                        short otherY = 0;
+                        while (!notHappyEither)
+                        {
+                            otherY = (short)rand.Next(0, gridHeight - 1);
+                            otherX = (short)rand.Next(0, gridWidth - 1);
+                            notHappyEither = !shellingGrid.Grid[otherY, otherX].HappyHere();
+                                //||
+                                //shellingGrid.Grid[otherY, otherX].Type == TypeEnum.Blank;
+                        }
+
+                        var temp = left;
+                        //temp.yLoc = y;
+                        //temp.xLoc = x;
+
+                        shellingGrid.Grid[y, x] = shellingGrid.Grid[otherY, otherX];
+                        shellingGrid.Grid[y, x].yLoc = y;
+                        shellingGrid.Grid[y, x].xLoc = x;
+
+                        shellingGrid.Grid[otherY, otherX] = temp;
+                        shellingGrid.Grid[otherY, otherX].yLoc = otherY;
+                        shellingGrid.Grid[otherY, otherX].xLoc = otherX;
+                    }
+
+                        x++;
+                }
+
+                x = 0;
+                y++;
+            }
+
+            randomizations++;
+        }
+
+        static bool IsEverybodyHappy()
+        {
+            short x = 0;
+            short y = 0;
+            bool everyoneIsHappy = true;
+
+            while (y < shellingGrid.GridHeight)
+            {
+                while (x < shellingGrid.GridWidth)
+                {
+                    //Console.WriteLine($"Checking if we need to move: y:{y}, x:{x}");
+
+                    if (shellingGrid.Grid[y, x].Type != TypeEnum.Blank)
+                    {
+                        if (!shellingGrid.Grid[y, x].HappyHere())
+                        {
+                            return false;
+                        }
+                    }
+                    x++;
+                }
+                x = 0;
+                y++;
+            }
+            return true;
         }
 
         static short IterateGrid()
@@ -107,7 +375,64 @@ namespace ShellingModel
             return moves;
         }
 
-        static void PopulateGrid()
+        static void PopulateGridFromFile()
+        {
+            ShellingObject shellingObject;
+
+            short w = 0;
+            short h = 0;
+            short index = 0;
+
+            char randomType;
+
+            while (h < gridHeight)
+            {
+                while (w < gridWidth)
+                {
+                    randomType = primingString[index];
+
+                    switch (randomType)
+                    {
+                        case 'X':
+                            shellingObject = new X(affinity, w, h, ref shellingGrid);
+                            shellingGrid.Grid[h, w] = shellingObject;
+                            shellingObject = new X(affinity, w, h, ref shellingGridOrig);
+                            shellingGridOrig.Grid[h, w] = shellingObject;
+                            break;
+                        case 'Y':
+                            shellingObject = new Y(affinity, w, h, ref shellingGrid);
+                            shellingGrid.Grid[h, w] = shellingObject;
+                            shellingObject = new Y(affinity, w, h, ref shellingGridOrig);
+                            shellingGridOrig.Grid[h, w] = shellingObject;
+                            break;
+                        case 'O':
+                            shellingObject = new O(affinity, w, h, ref shellingGrid);
+                            shellingGrid.Grid[h, w] = shellingObject;
+                            shellingObject = new O(affinity, w, h, ref shellingGridOrig);
+                            shellingGridOrig.Grid[h, w] = shellingObject;
+                            break;
+                        case 'T':
+                            shellingObject = new T(affinity, w, h, ref shellingGrid);
+                            shellingGrid.Grid[h, w] = shellingObject;
+                            shellingObject = new T(affinity, w, h, ref shellingGridOrig);
+                            shellingGridOrig.Grid[h, w] = shellingObject;
+                            break;
+                        case '_':
+                            shellingObject = new Blank(affinity, w, h, ref shellingGrid);
+                            shellingGrid.Grid[h, w] = shellingObject;
+                            shellingObject = new Blank(affinity, w, h, ref shellingGridOrig);
+                            shellingGridOrig.Grid[h, w] = shellingObject;
+                            break;
+                    }
+                    index++;
+                   w++;
+                }
+                w = 0;
+                h++;
+            }
+        }
+
+        static void PopulateGridRandom()
         {
             Array values = Enum.GetValues(typeof(TypeEnum));
             Random random = new Random();
@@ -130,10 +455,12 @@ namespace ShellingModel
                             shellingObject = new X(affinity, w, h, ref shellingGridOrig);
                             shellingGridOrig.Grid[h, w] = shellingObject;
                             break;
-                        //case TypeEnum.Y:
-                        //    shellingObject = new Y(affinity, w, h, ref shellingGrid);
-                        //    shellingGrid.Grid[h, w] = shellingObject;
-                        //    break;
+                        case TypeEnum.Y:
+                            shellingObject = new Y(affinity, w, h, ref shellingGrid);
+                            shellingGrid.Grid[h, w] = shellingObject;
+                            shellingObject = new Y(affinity, w, h, ref shellingGridOrig);
+                            shellingGridOrig.Grid[h, w] = shellingObject;
+                            break;
                         case TypeEnum.O:
                             shellingObject = new O(affinity, w, h, ref shellingGrid);
                             shellingGrid.Grid[h, w] = shellingObject;
